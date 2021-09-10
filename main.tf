5351a0e25ff557636eec03acd0f470fbc0f5badf
James Gawn <james@gawn.uk>
2021-09-10 21:07:57 +0100

    Add the azure provider as required

--- main.tf
+++ main.tf
@@ -1,8 +1,15 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.76.0"
+    }
+  }
   backend "azurerm" {
     resource_group_name  = "terraform"
     storage_account_name = "gawnbackend"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
   }
+
 }
